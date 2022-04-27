71778e3e0fb65bc2879ad2c9e4852eacea97f411
Siqi Yuan <yuansiqi0114@hotmail.com>
2022-04-26 23:11:08 -0400

    record command for plotting

---
+++ elink_rate_study/cmdlog.sh
@@ -0,0 +1,2 @@
+./plot_distribution.py $1 --config ../config/v7.config --nopad --stack --area all --newload
+./plot_distribution.py $1 --config ../config/v7.config --nopad --stack --area bysection --individual
