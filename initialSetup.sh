c9e6a443f4999152d9767a53ef60b4358d732320
sihyunjeon <shjeon@cern.ch>
2023-04-21 09:07:14 +0200

    add env setup script

---
+++ initialSetup.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+mkdir env
+cd env
+
+source /cvmfs/sft.cern.ch/lcg/views/LCG_95apython3/x86_64-centos7-gcc8-opt/setup.sh
+
+ENVNAME="bucoffeaenv"
+python -m venv ${ENVNAME}
+source ${ENVNAME}/bin/activate
+export PYTHONPATH=${PWD}/${ENVNAME}/lib/python3.6/site-packages:$PYTHONPATH
+git clone git@github.com:bu-cms/bucoffea.git
+python -m pip install -e bucoffea
+pip install matplotlib==3.1
+pip install mplhep==0.1.5
+cd ..
+mv initialSetup.sh env/
+
+echo 'source /cvmfs/sft.cern.ch/lcg/views/LCG_95apython3/x86_64-centos7-gcc8-opt/setup.sh' >> setup.sh
+echo 'ENVNAME="bucoffeaenv"' >> setup.sh
+echo 'source env/${ENVNAME}/bin/activate' >> setup.sh
+echo 'export PYTHONPATH=env/${ENVNAME}/lib/python3.6/site-packages:$PYTHONPATH' >> setup.sh
